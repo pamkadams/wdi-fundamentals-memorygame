a88d00496bbb1760084180a53de2928963dedbb0
Pamela Adams <pkadams@PamMacBook.local>
2019-09-02 15:51:31 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,46 @@
 //array to store the cards
-let cards = ['queen', 'queen', 'king', 'king'];
+let cards = [
+	{
+		rank: 'queen',
+		suit: 'hearts',
+		cardImage: 'images/queen-of-hearts.png'
+	},
+
+	{
+		rank: 'queen',
+		suit: 'diamonds',
+		cardImage: 'images/queen-of-diamonds.png'
+	},
+	
+	{
+		rank: 'king',
+		suit: 'hearts',
+		cardImage: 'images/king-of-hearts.png'
+	},
+	
+	{
+
+		rank: 'king',
+		suit: 'diamonds',
+		cardImage: 'images/king-of-diamonds.png'
+	}
+
+];
 //empty array to store cards in play
 let cardsInPlay =[];
 
 //check if two cards match
 function checkForMatch(){
-	if (cardsInPlay[0]===cardsInPlay[1]) console.log("You found a match!");
-	else console.log("Sorry, try again.");
+	if (cardsInPlay[0]===cardsInPlay[1]) alert("You found a match!");
+	else alert("Sorry, try again.");
 	//};
 };
 //function to flip and check if 
 function flipCard(cardId){
-	console.log("User flipped " +cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " +cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+	cardsInPlay.push(cards[cardId].rank);
 	if (cardsInPlay.length === 2) checkForMatch();
 };
 flipCard(0);
